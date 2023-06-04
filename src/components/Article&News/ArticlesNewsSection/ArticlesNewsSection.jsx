7fbe3eeaf09ddb2ef3ffbfe3c5397aeda5d9d93f
masumeh <khanimasume88@gmail.com>
2023-06-04 14:05:50 +0330

    create ArticleNewsSection component

---
+++ src/components/Article&News/ArticlesNewsSection/ArticlesNewsSection.jsx
@@ -0,0 +1,25 @@
+import ArticleCard from '../../ArticleCard/ArticleCard'
+import styles from './ArticlesNewsSection.module.scss'
+const ArticlesNewsSection = ({articlesNewsTitle}) => {
+  return (
+    
+<div className={styles.container}>
+    <div className={styles.articlesNewsTitleContainer}>
+      <span className={styles.articlesNewsTitle}>{articlesNewsTitle}kkk</span>  
+    </div>
+  <div className={styles.articlesNewsSection}>
+    <ArticleCard />
+    <ArticleCard />
+    <ArticleCard />
+  </div>
+  <div className={styles.articlesNewsBtnContainer}>
+    <button className={styles.articlesNewsBtn}>بیشتر</button>
+  </div>
+</div>
+
+  )
+}
+
+export default ArticlesNewsSection
+
+
