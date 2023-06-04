ecad92bd3cdaf95e17665b37f6369d122c113f40
masumeh <khanimasume88@gmail.com>
2023-06-04 09:55:53 +0330

    create articleNews component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 
+import ArticleNews from './components/Article&News/ArticleNews'
 import ArticleCard from './components/Article-Card/ArticleCard'
+import ArticleCards from './components/Article-Cards/ArticleCards'
 function App() {
   return (
     <>
       <ArticleCard/>
+      <ArticleCards/>
+      <ArticleNews/>
     </>
   )
 }
---
+++ src/components/Article&News/ArticleNews.jsx
@@ -0,0 +1,10 @@
+
+const ArticleNews = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ArticleNews
