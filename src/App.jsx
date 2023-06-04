00408ea8d59aec0b070dd52b5d82501337876295
masumeh <khanimasume88@gmail.com>
2023-06-04 14:18:35 +0330

    delete Article-card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
+import ArticlesNewsSection from "./components/Article&News/ArticlesNewsSection/ArticlesNewsSection"
 
-import ArticleNews from './components/Article&News/ArticleNews'
-import ArticlesNewsSection from './components/Article&News/ArticlesNewsSection/ArticlesNewsSection'
-import ArticleCard from './components/ArticleCard/ArticleCard'
+
 
 function App() {
   return (
     <>
-    
-      <ArticleNews/>
-      <ArticleCard/>
-      <ArticlesNewsSection/>
+    <ArticlesNewsSection/>
     </>
   )
 }
